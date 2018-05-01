b84199149adc3fb69a33d8baa21d0165a3907a4f
DmytroRomaniv <d.romanivvv@gmail.com>
2018-05-01 13:57:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,17 +11,9 @@ namespace InheritanceTask
     {
         static void Main(string[] args)
         {
-            Cylinder newCircle;
-            while (true)
-            {
-                double width = double.Parse(Console.ReadLine().ToString());
-                double radius = double.Parse(Console.ReadLine().ToString());
-                double height = double.Parse(Console.ReadLine().ToString());
-                newCircle = new Cylinder("1", "1", width, radius,height);
-                newCircle.Show();
-                Console.WriteLine(newCircle.Volume());
-                Console.WriteLine(newCircle.Area());
-            }
+            Calculation calc = new Calculation();
+            calc.OutputCalculation();
+            Console.ReadKey();
         }
     }
 }
