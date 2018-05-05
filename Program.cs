fe268a71aaff9c089997910e432dfa180dbb10fc
Дмитро Романів <d.romanivvv@gmail.com>
2018-05-05 18:03:20 +0200

    Exception

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InheritanceTask.Classes;
 
-namespace ConsoleApplication68
+namespace InheritanceTask
 {
     class Program
     {
